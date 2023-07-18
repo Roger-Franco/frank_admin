efe0826944e890925fd6c71663b4936a0ad45925
Roger Franco <rogerfrancogalt@gmail.com>
2023-07-18 16:49:28 -0400

    Função de deletar

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,22 +2,31 @@ import { Link, useLocation } from 'react-router-dom';
 import { userColumns, userRows } from '../../datatablesource';
 import './datatable.scss'
 import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
+import { FilterDrama } from '@mui/icons-material';
 
 const Datatable = () => {
+  const [data, setData] = useState(userRows)
   // const location = useLocation()
   // const path = location.pathname.split('/')[1]
+
+  const handleDelete = (id) => {
+    const filteredData = data.filter((item) => item.id !== id)
+    setData(filteredData)
+  }
+  console.log(data, 'data')
 
   const actionColumn = [{
     field: "action",
     headerName: "Action",
     width: 200,
-    renderCell: () => {
+    renderCell: (params) => {
       return (
         <div className="cellAction">
           <Link to='/users/test' style={{textDecoration: "none"}}>
             <div className="viewButton">View</div>
           </Link>
-          <div className="deleteButton">Delete</div>
+          <div onClick={() => handleDelete(params.row.id)} className="deleteButton">Delete</div>
         </div>
       );
     },
@@ -33,7 +42,7 @@ const Datatable = () => {
       </div>
       <DataGrid
       className='datagrid'
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
